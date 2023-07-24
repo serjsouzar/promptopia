ae92b588fddbc43f761fae3c21b282ed04b411b4
Sergio Souza <serjsouza93@gmail.com>
2023-07-24 16:55:44 -0300

    handleEdit and handleDelete done

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,11 +2,13 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+
+  const router = useRouter()
 
   const { data: session } = useSession();
 
@@ -23,11 +25,27 @@ const MyProfile = () => {
     if(session?.user.id) fetchPosts();
   }, []);
 
-  //todo
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  };
 
-  //todo
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+  
+    if(hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`,{
+          method: "DELETE"
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+
+        setPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  };
 
   return (
     <Profile
